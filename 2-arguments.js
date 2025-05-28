2d560dd79ac5d288ed0d723dc6017cfea47da646
ebenrich29 <ebenrich29@gmail.com>
2025-05-28 17:42:20 +0000

    2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// argument
+let satiation = 200
+if (satiation >= 100)
+    console.log("No agument found");
+else if (satiation < 100)
+    console.log("Agument found");
+else 
+console.log("Agument found");
+    
+
