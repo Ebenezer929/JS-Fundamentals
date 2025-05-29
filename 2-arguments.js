c97829e1e393ea09e2a5cddd4bab5ef9b76256a6
ebenrich29 <ebenrich29@gmail.com>
2025-05-29 11:15:55 +0000

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -9,3 +9,25 @@ else if (k <= 16) {
 else {
     console.log('Arguments found');
 }
+// aruments
+let M = 115;
+if (M == 18) {
+    console.log('No argument found');
+}
+else if (M <= 16) {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}
+// argument
+let Y = 115;
+if (Y == 115) {
+    console.log('No argument found');
+}
+else if (Y <= 16) {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}
