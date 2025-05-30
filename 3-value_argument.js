d3567c0a358e5e8909986da1c0f987eea5c3015d
ebenrich29 <ebenrich29@gmail.com>
2025-05-30 18:36:43 +0000

    3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,12 @@
-// Arguments
-let argpass = 45;
+// Argument 3
+const args = process.argv.slice(2)
 
-if(argpass = 45) {
-    console.log('No argument');
+if (args.lenght === 0) {
+    console.log('No arguments');
+} 
+else if (args.lenght === 1) {
+    console.log('Argument found');
 }
 else {
-    console.log('Argument');
+    console.log('Arguments found');
 }
