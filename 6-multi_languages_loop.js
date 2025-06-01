6f30bb6d0c91d3fe167fa114933e508b52c44f3c
ebenrich29 <ebenrich29@gmail.com>
2025-06-01 19:21:55 +0000

    6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 // writing multiple loops
-const words = ["C is fun", "python is fun","JavaScrip is amazimg"]
+const words = ["C is fun", "Python is fun","JavaScript is amazimg"]
 
 for (let i = 0; i < words.length; i++) {
     console.log(words[i]);
